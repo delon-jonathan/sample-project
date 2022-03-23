334c87d310223189956af1fd576f9d9a45dfb18b
jonathanlazar <jonathanlazar17@gmail.com>
2022-03-23 20:42:18 +0800

    Renaming file

--- sample-project/MainForm.cs
+++ sample-project/MainForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace sample_project
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
         }
